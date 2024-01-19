433e0d23a8b12116eff923cfb7d49d8a8736f15f
Nietsnie-beep <al02982380@tecmilenio.mx>
2024-01-18 21:22:49 -0600

    Create a reusable dialog class

--- app/src/main/java/com/example/cursolinkedin/MainActivity.kt
+++ app/src/main/java/com/example/cursolinkedin/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.cursolinkedin
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AlertDialog
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -14,7 +17,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cursolinkedin.ui.theme.CursoLinkedinTheme
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.compose.material3.AlertDialog
 import com.google.android.material.snackbar.Snackbar
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,6 +31,9 @@ class MainActivity : AppCompatActivity() {
         var btn1 : Button = findViewById(R.id.btn1)
         var btn2 : Button = findViewById(R.id.btn2)
         var btn3 : Button = findViewById(R.id.btn3)
+
+
+
 
         btn1.setOnClickListener {
             val defaultSnackbar = Snackbar.make(llmain, "Hii, dream developers", Snackbar.LENGTH_LONG)
@@ -39,5 +47,28 @@ class MainActivity : AppCompatActivity() {
             val actionSnackbar = Snackbar.make(llmain, "Action developer", Snackbar.LENGTH_LONG)
             actionSnackbar.show()
         }
+        showEditTextDialog()
     }
+
+    private fun showEditTextDialog(){
+        var tv_textView : TextView = findViewById(R.id.tv_textView)
+
+        tv_textView.setOnClickListener{
+
+            val builder = AlertDialog.Builder(this)
+            val inflater = layoutInflater
+            val dialogLayout = inflater.inflate(R.layout.edit_text_layout, null)
+            val editText = dialogLayout.findViewById<EditText>(R.id.et_editText)
+
+            with(builder){
+                setTitle("Enter some text")
+                setPositiveButton("ok"){
+                        dialog, wich -> tv_textView.text = editText.text.toString()
+                }
+                setView(dialogLayout)
+                show()
+            }
+        }
+    }
+
 }
